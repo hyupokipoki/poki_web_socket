1a26952fa3b26e726f4c4d6e07cc5cac11fd6550
ParkBeomMin <club20608@gmail.com>
2019-05-22 01:28:47 +0900

    modify host

--- src/plugins/socketPlugin.js
+++ src/plugins/socketPlugin.js
@@ -4,22 +4,18 @@ import io from 'socket.io-client';
 const socket = io('http://211.51.76.18:8082');
 
 
+
 const SocketPlugin = {
   install(vue) {
     vue.mixin({
     });
-
     vue.prototype.$sendMessage = ($payload) => {
-      vue.prototype.$sendMessage = ($payload) => {
-        socket.emit('message', {
-          msg: $payload.msg,
-          name: $payload.name,
-        });    };
-  
-
+      socket.emit('message', {
+        msg: $payload.msg,
+        name: $payload.name,
+      });    };
     // 인스턴스 메소드 추가
     vue.prototype.$socket = socket;
   },
 };
-
 Vue.use(SocketPlugin);
