51114b950b4fc7ac7807d4b0ac0fd7b8816fb88b
ParkBeomMin <club20608@gmail.com>
2019-05-22 01:25:31 +0900

    modify host

--- src/plugins/socketPlugin.js
+++ src/plugins/socketPlugin.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import io from 'socket.io-client';
 
-const socket = io('http://127.0.0.1:8082');
+const socket = io('http://211.51.76.18:8082');
 
 
 const SocketPlugin = {
@@ -10,10 +10,12 @@ const SocketPlugin = {
     });
 
     vue.prototype.$sendMessage = ($payload) => {
-      socket.emit('message', {
-        msg: $payload.msg,
-        name: $payload.name,
-      });    };
+      vue.prototype.$sendMessage = ($payload) => {
+        socket.emit('message', {
+          msg: $payload.msg,
+          name: $payload.name,
+        });    };
+  
 
     // 인스턴스 메소드 추가
     vue.prototype.$socket = socket;
